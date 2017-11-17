703e704d385e20c06dbbd3914210cf906c7835a3
jmcoder1 <jmcoder@outlook.com>
2017-11-17 17:59:38 +0000

    Add files via upload

---
+++ content.js
@@ -0,0 +1,24 @@
+var elements = document.getElementsByTagName('*');
+
+for var(i = 0; i < elements.length; i++) {
+	var element = elements[i];
+
+	for(var j = 0; j < element.childNodes.length; j++) {
+		var node = element.childNodes[j];
+
+		if(node.nodeType === 3) {
+			var text = node.nodeValue;
+			var replacedText1 = text.replace(/Game/gi, 'GAME__');
+			var replacedText2 = text.replace(/Gameplay/gi, 'GAME STUFF');
+			
+			if (replacedText1 !== text) {
+				element.replaceChild(document.createTextNode(replacedText1), node);
+			}
+
+			if (replacedText2 !== text) {
+				element.replaceChild(document.createTextNode(replacedText2), node);
+
+			}
+		}	
+	}
+}
